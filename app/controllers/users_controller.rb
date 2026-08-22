5c3d31d2768e35c35c7b239ea1bad926bb20f5dd
Abhishek Nagare <abhishek.nagare@definelabs.com>
2026-08-22 23:57:21 +0530

    Backup before complete UI redesign~

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,15 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
+  def index
+    @users = User.all
+  end
+
   def edit
     @user = User.find(params[:id])
   end
