4f33d6499d6030bb20426db4143f9445eafd6de7
Abhishek Nagare <abhishek.nagare@definelabs.com>
2026-08-24 23:58:37 +0530

    Move authentication methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
   def main_page
     render "This is the main page from alpha-blog"
   end
