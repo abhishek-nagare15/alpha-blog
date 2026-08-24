45ae13422bb62a5aa9fa0585e83ef8a263e45a0d
Abhishek Nagare <abhishek.nagare@definelabs.com>
2026-08-24 23:47:16 +0530

    Add user authentication helpers and conditional navigation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def new
     @user = User.new
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 6)
   end
 
@@ -14,12 +14,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
       redirect_to @user
@@ -30,18 +27,23 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
     if @user.save
-      flash[:notice] = "Welcome to alpha blog #{@user.username}, you have successfully signed in"
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you have successfully signed in"
       redirect_to articles_path
     else
-      render 'new'
+      render "new", status: :unprocessable_entity
     end
-
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-
 end
