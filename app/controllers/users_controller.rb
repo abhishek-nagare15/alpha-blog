3ebfd9b53276841446828272fa2c5dd7ffc4abb5
Abhishek Nagare <abhishek.nagare@definelabs.com>
2026-08-26 20:49:09 +0530

    Add user account deletion with dependent articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
-  before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def new
     @user = User.new
@@ -25,6 +25,13 @@ class UsersController < ApplicationController
     else
       render "edit", status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Your account and all associated articles have been successfully deleted"
+    redirect_to articles_path
   end
 
   def create
